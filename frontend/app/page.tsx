d0094bf7e2c3b3493567dc51f5d1f2d28bcfd592
Stefan Todorov <79049907+StefanTodorov32@users.noreply.github.com>
2024-06-17 20:53:34 +0300

    nextjs initial commit

---
+++ frontend/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col ">
+      <header className="flex justify-center items-center h-20 bg-gray-800 text-white">
+        <h1 className="text-2xl">Welcome to Next.js</h1>
+      </header>
+    </main>
+  );
+}
