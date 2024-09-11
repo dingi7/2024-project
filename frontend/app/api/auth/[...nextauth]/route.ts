e7798ceb599888a0b3994917fc36e8ae596ef969
Kamen Kanev <kamenkanev88@gmail.com>
2024-09-11 13:26:19 +0300

    Update route.ts

--- frontend/app/api/auth/[...nextauth]/route.ts
+++ frontend/app/api/auth/[...nextauth]/route.ts
@@ -4,7 +4,7 @@ import NextAuth, { NextAuthOptions } from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github';
 import { userSignIn } from '../../requests';
 
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
     providers: [
         GitHubProvider({
             clientId: process.env.GITHUB_ID ?? '',
